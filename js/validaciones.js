a11c5c3c65c6fd1e41b245998b7de1d93e598d37
nattygd <nattystoessel13@gmail.com>
2023-09-17 06:09:30 -0300

    Reportajes y comentar terminado

--- js/validaciones.js
+++ js/validaciones.js
@@ -77,7 +77,7 @@ $(document).ready(function() {
         },
         submitHandler: (form) => {
             const data = Object.fromEntries(new FormData(form));
-            window.location.href = "home.html";
+            window.location.href = "paginaUsuario.html";
             console.log('Validado');
             console.log(data);
         }
@@ -148,7 +148,7 @@ $(document).ready(function() {
         },
         submitHandler: (form) => {
             const data = Object.fromEntries(new FormData(form));
-            window.location.href = "home.html";
+            window.location.href = "paginaUsuario.html";
             console.log('Validado');
             console.log(data);
         }
